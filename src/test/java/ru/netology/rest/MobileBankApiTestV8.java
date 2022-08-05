7c2b05d0a19504fef62fff661509429c132e302d
Антон Попов <aspirevx74@gmail.com>
2022-08-05 13:07:19 +0300

    NewTest

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV8.java
@@ -0,0 +1,19 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class MobileBankApiTestV8 {
+    @Test
+    void shouldReturnDemoAccountsCurrencyRUB() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .body("[0].currency", equalTo("RUB"));
+    }
+}
---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV9.java
@@ -0,0 +1,19 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class MobileBankApiTestV9 {
+    @Test
+    void shouldReturnDemoAccountsCurrencyUSD() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+                .body("[1].currency", equalTo("USD"));
+    }
+}
